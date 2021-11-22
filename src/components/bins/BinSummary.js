152d3c56a789581169a72d852f861e725b960ee6
Fatima Jangda <59586270+fjangda7@users.noreply.github.com>
2021-11-22 17:39:39 -0500

    changes
    bin changes

--- src/components/bins/BinSummary.js
+++ src/components/bins/BinSummary.js
@@ -5,7 +5,7 @@ const BinSummary = ({bin}) => {
         <div className="card z-depth-0 project-summary">
         <div className="card-content grey-text text-darken-3">
           <span className="card-title ">Bin {bin.number}</span>
-          <p>Posted by The Net Ninja</p>
+          <p>Posted by Fatima</p>
           <p className="grey-text">3rd September, 2am</p>
         </div>
       </div>
--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -7,7 +7,7 @@ const SignedInLinks = () => {
       <ul className="right">
         <li><NavLink to='/create'>New Project</NavLink></li>
         <li><NavLink to='/'>Log Out</NavLink></li>
-        <li><NavLink to='/' className="btn btn-floating pink lighten-1">NN</NavLink></li>
+        <li><NavLink to='/' className="btn btn-floating pink lighten-1">FJ</NavLink></li>
       </ul>
     </div>
   )
